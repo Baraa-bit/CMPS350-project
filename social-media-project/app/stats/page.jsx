956da1d2d5b56b91535f4ea338955cab80d938c5
y5u4u3 <ys2005340@qu.edu.qa>
2026-05-02 01:42:54 +0300

    Fixed the page class to fetch data from the api

--- social-media-project/app/stats/page.jsx
+++ social-media-project/app/stats/page.jsx
@@ -1,5 +1,33 @@
-import SocialDashboard from "@/components/SocialDashboard";
+"use client";
+import { useState, useEffect } from "react";
+import StatItem from "@/components/StatItem";
+import Header from "@/components/stats/Header";
+import TotalsRow from "@/components/stats/TotalsRow";
+import Stats from "@/components/stats/Stats";
 
-export default function StatsPage() {
-  return <SocialDashboard />;
+function Page() {
+  const [statsData, setStatsData] = useState(null);
+
+  useEffect(() => {
+    // Fetch stats data from API and update state
+    fetch("http://localhost:3000/api/stats")
+      .then((response) => response.json())
+      .then((data) => {
+        //Update state with fetched datas
+        setStatsData(data);
+      })
+      .catch((error) => console.error("Error fetching stats:", error));
+  }, []);
+
+  useEffect(() => {
+    console.log("statsData: ", statsData);
+  }, [statsData]);
+
+  return (
+    <div>
+      <Stats data={statsData} />
+    </div>
+  );
 }
+
+export default Page;
